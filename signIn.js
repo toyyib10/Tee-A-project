3e0d440ea1d9820e9bf375d1fb480131d6ebe1a0
Yekeen Toyyib <yekeentoyyib@gmail.com>
2022-07-31 19:57:03 +0100

    i worked on sign in and sign up js

--- signIn.js
+++ signIn.js
@@ -1,20 +1,63 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
+let saved_Info = []
+let username;
+let email;
+let check = false;
+let password;
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+const loadData = () => {
+  saved_Info = JSON.parse(localStorage.tee_AInformation)
+}
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+const logIn = () => {
+  submitButton.type = "submit"
+  if (user_mail.value == "" || passWord.value == "") {
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+      'use strict'
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+    alertDanger.style.display = "none";
+  }
+  else{
+    submitButton.type = "button"
+    for (let index = 0; index < saved_Info.length; index++) {
+      let umail = user_mail.value;
+      let pWord = passWord.value;
+      username = saved_Info[index].user.user_name;
+      email = saved_Info[index].user.e_mail;
+      password = saved_Info[index].user.pass_word;
+      if ((umail == username || umail == email) && pWord == password) {
+        check = true;
+        break;
+      }
+      else{
+        continue;
+      }
+    }
+    if (check == true) {
+      window.location.href = "user-landing-page.html";
+      user_mail.value = "";
+      alertDanger.style.display = "none";
+    }
+    else if (check == false) {
+      alertDanger.style.display = "block"
+    }
+  }
+}
+const displayNone = () => {
+  alertDanger.style.display = "none";
+}
--- signUp.js
+++ signUp.js
@@ -1,20 +1,67 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
+let fName;
+let uName;
+let email;
+let pNumber;
+let pWord;
+let unsaved_info = [];
+let termCheck = false;
+let info;
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+const loadData = () => {
+  if (localStorage.tee_AInformation){
+    unsaved_info = JSON.parse(localStorage.tee_AInformation)
+    console.log(unsaved_info)
+  }
+}
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+invalidCheck2.addEventListener("click",function(){
+  termCheck =  true;
+});
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+const createAccount = () =>{
+  fName = fullName.value;
+  uName = userName.value;
+  email = eMail.value;
+  pNumber = phoneNumber.value;
+  pWord = passWord.value;
+  if (fName == "" || uName ==  "" || email == "" || pNumber == "" || pWord == "" || termCheck == false){
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+      'use strict'
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+  }
+  else{
+    info = {
+      user: {
+        full_name : fName,
+        user_name : uName,
+        e_mail : email,
+        phone_number : pNumber,
+        pass_word : pWord,
+      }
+    }
+    unsaved_info.push(info)
+    localStorage.tee_AInformation = JSON.stringify(unsaved_info)
+    window.location.href = "user-landing-page.html"
+    fullName.value = "";
+    userName.value = "";
+    eMail.value = "";
+    phoneNumber.value = "";
+    passWord.value = "";
+  }
+}
